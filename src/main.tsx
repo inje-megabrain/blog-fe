1a54b4c1fa6792b5942180c759397c51cc214677
byungjin <byungjin.dev@gmail.com>
2022-10-23 16:44:52 +0900

    chore: src/main.tsx 변경사항 삭제
    변경사항
     - infinitePage Page 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
-import InfiniteScroll from './components/InfinitePage/InfiniteScroll';
 import ErrorPage from './pages/Error/Error';
 import Root from './pages/Main/Main';
 import TestChildrenPage from './pages/TestChildren/TestChildren';
@@ -15,11 +14,6 @@ const router = createBrowserRouter([
   {
     path: 'children',
     element: <TestChildrenPage />,
-  },
-  /** Just Test Infinite Scroll. Don't commit this */
-  {
-    path: 'infinitePage',
-    element: <InfiniteScroll />,
   },
 ]);
 
