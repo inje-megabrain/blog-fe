55a9d96814871e929ef6a8804b9b0be613bf4a16
narinn-star <skfls2618@naver.com>
2022-10-27 16:04:18 +0900

    feat: Profile FadeModal 기능 추가
    Details
     - FadeModal 분리

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,35 +1,9 @@
 import React from 'react';
-import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import styled from 'styled-components';
-
-const ProfileStyle = styled.div`
-  position: absolute;
-  top: 40%;
-  left: 50%;
-`;
-const imgStyle = {
-  width: '200px',
-  height: '200px',
-  margin: 'auto 45px',
-};
-
-const boxStyle = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '300px',
-  bgcolor: 'background.paper',
-  border: '0px solid #000000',
-  boxShadow: 20,
-  borderRadius: '40px',
-  padding: '40px',
-};
+import ProfileStyle from './Profile.module.css';
+import FadeModal from './FadeModal';
 
 function Profile() {
   const [open, setOpen] = React.useState(false);
@@ -37,34 +11,26 @@ function Profile() {
   const handleClose = () => setOpen(false);
 
   return (
-    <ProfileStyle>
+    <div className={ProfileStyle.profileStyle}>
       <Button className="btn" onClick={handleOpen}>
         Open Profile
       </Button>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <Box className="box" sx={boxStyle}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              narinn-star's Profile
-            </Typography>
-            <img src="src\assets\Cookie_Monster.png" style={imgStyle}></img>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Introduce YourSelf in this box.
-            </Typography>
-          </Box>
-        </Fade>
-      </Modal>
-    </ProfileStyle>
+      <FadeModal open={open} handleClose={handleClose}>
+        <Box className={ProfileStyle.boxStyle}>
+          <Typography id="transition-modal-title" variant="h6" component="h2">
+            narinn-star's Profile
+          </Typography>
+          <img
+            src="src\assets\Cookie_Monster.png"
+            className={ProfileStyle.imgStyle}
+            alt="PROFILE IMAGE"
+          ></img>
+          <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+            Introduce YourSelf in this box.
+          </Typography>
+        </Box>
+      </FadeModal>
+    </div>
   );
 }
 
